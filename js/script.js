35f7c546f12a8cd962d80776c1e9f7a9035069bf
Sergey Kirik <onceagain@ua.fm>
2018-05-24 23:00:16 +0300

    24/05

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// let timerId = setInterval(function() {
+//     alert( "тик" );
+// }, 2000);
+$('.slider').slick({
+    dots: true,
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+    arrows: false,
+    infinite: true,
+    speed: 1000,
+});
