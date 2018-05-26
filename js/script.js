3515cea1e24b9df9fc77649bc5da0b00ef60f9a1
Sergey Kirik <onceagain@ua.fm>
2018-05-26 16:35:26 +0300

    add slowmo for a

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 // let timerId = setInterval(function() {
 //     alert( "тик" );
 // }, 2000);
+
+$(function(){
+    $('a[href^="#"]').on('click', function(event) {
+        // отменяем стандартное действие
+        event.preventDefault();
+
+        let sc = $(this).attr("href"),
+            dn;
+        if (sc != "#" ) {
+            dn = $(sc).offset().top;
+        }
+        else {
+            dn = 0;
+        }
+        /*
+        * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+        * dn - определяем положение блока на странице
+        */
+
+        $('html, body').animate({scrollTop: dn}, 1000);
+
+        /*
+        * 1000 скорость перехода в миллисекундах
+        */
+    });
+});
+
 $('.slider').slick({
     dots: true,
     // autoplay: true,
@@ -9,3 +36,4 @@ $('.slider').slick({
     infinite: true,
     speed: 1000,
 });
+
